828a31d7747b30d103498205cc668de205518dcf
Wasif <wasif.ibrahim29@gmail.com>
2020-01-11 15:46:41 +0500

    Initial commit

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World!")
+    my_dict={ 'insert_me':"Hello I am from views.py!"}
+    return render(request,'index.html',context=my_dict)
 
 def challenge(request):
     return HttpResponse("<h2>My First Challenge<h2>")
