1d0f76bc06095697891ef368598fee46cbb81d0b
Wasif <wasif.ibrahim29@gmail.com>
2020-01-11 14:09:38 +0500

    second commit

--- first_project/first_app/urls.py
+++ first_project/first_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from first_app import views
 
 urlpatterns = [
-    path('/', views.index,name="index"),
+    path('', views.index,name="index"),
     path('challenge/',views.challenge,name="challenge"),
     path('help/',views.home,name="help"),
 ]
